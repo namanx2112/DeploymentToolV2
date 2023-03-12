cbaffd68a2a49fecb6832b6304a533f1628449f5
siddugadag <siddu.gadag@gmail.com>
2023-03-12 15:20:51 +0530

    added vendor api

--- Model/Vendor.cs
+++ Model/Vendor.cs
@@ -7,24 +7,28 @@ namespace DeploymentTool.Model
 {
     public class Vendor
     {
-        public int VendorId { get; set; }
-        public string VendorName { get; set; }
-        public int TechComponentID { get; set; }
-        public string VendorDescription { get; set; }
-        public string VendorEmail { get; set; }
-        public string VendorAddress { get; set; }
-        public string VendorPhone { get; set; }
-        public string VendorContactPerson { get; set; }
-        public string VendorWebsite { get; set; }
-        public string VendorCountry { get; set; }
-        public DateTime? VendorEstablished { get; set; }
-        public string VendorCategory { get; set; }
-        public string VendorContact { get; set; }
-        public int? CreatedBy { get; set; }
-        public int? UpdatedBy { get; set; }
-        public DateTime? CreatedOn { get; set; }
-        public DateTime? UpdatedOn { get; set; }
-        public bool? Deleted { get; set; }
+        public int aVendorId { get; set; }
+        public string tVendorName { get; set; }
+        public int nTechComponentID { get; set; }
+
+        public int nBrandID { get; set; }
+        public string tVendorDescription { get; set; }
+        public string tVendorEmail { get; set; }
+        public string tVendorAddress { get; set; }
+        public string tVendorPhone { get; set; }
+        public string tVendorContactPerson { get; set; }
+        public string tVendorWebsite { get; set; }
+        public string tVendorCountry { get; set; }
+        public DateTime? tVendorEstablished { get; set; }
+        public string tVendorCategory { get; set; }
+        public string tVendorContact { get; set; }
+        public int? nCreatedBy { get; set; }
+        public int? nUpdatedBy { get; set; }
+        public DateTime? dtCreatedOn { get; set; }
+        public DateTime? dtUpdatedOn { get; set; }
+        public bool? bDeleted { get; set; }
+        public int? nPageSize { get; set; }
+        public int? nPageNumber { get; set; }
     }
 
 }
